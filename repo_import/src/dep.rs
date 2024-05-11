537a40b92daf0986b839d888cb1d3402bea48a7a
xizheyin <xizheyin@smail.nju.edu.cn>
2024-05-11 15:44:59 +0800

    fix bugs in repo_import(parse dependencies)
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>

--- repo_import/src/dep.rs
+++ repo_import/src/dep.rs
@@ -1,7 +1,11 @@
+use std::path::PathBuf;
+
 use git2::Repository;
 use git2::{TreeWalkMode, TreeWalkResult};
-use model::crate_info::UVersion;
+use model::crate_info::{ApplicationVersion, LibraryVersion, UProgram, UVersion};
 use toml::Value;
+
+use crate::utils::{get_program_by_name, PROGRAM_HASHMAP};
 
 #[allow(unused)]
 #[derive(Debug, Default, Clone)]
@@ -12,17 +16,17 @@ pub(crate) struct Dependencies {
 }
 
 /// a git repo contains different crates
-pub(crate) fn extract_all_tags(repo: &Repository) -> Vec<UVersion> {
-    let versions: Vec<UVersion> = vec![];
+pub(crate) fn parse_all_versions_of_a_repo(repo: &Repository) -> Vec<UVersion> {
+    let mut versions: Vec<UVersion> = vec![];
     let tags = repo.tag_names(None).expect("Could not retrieve tags");
 
     for tag_name in tags.iter().flatten() {
         let obj = repo
             .revparse_single(&("refs/tags/".to_owned() + tag_name))
             .expect("Couldn't find tag object");
-        println!("{:?}", obj);
+        //println!("{:?}", obj);
 
-        // 尝试将对象作为注释标签处理
+        // convert annotated and light-weight tag into commit
         let commit = if let Some(tag) = obj.as_tag() {
             tag.target()
                 .expect("Couldn't get tag target")
@@ -38,32 +42,47 @@ pub(crate) fn extract_all_tags(repo: &Repository) -> Vec<UVersion> {
 
         // FIXME: deal with different formats
         // parse the version, walk all the packages
-        let _all_packages_dependencies = find_and_parse_cargo_toml(repo, &tree);
+        let all_packages_dependencies = parse_a_repo_of_a_version(repo, &tree);
+        //debug!("{:?}", all_packages_dependencies);
+        for dependencies in all_packages_dependencies {
+            let name = dependencies.crate_name;
+            let version = dependencies.version;
+            //println!("{:#?}", PROGRAM_HASHMAP.lock().unwrap());
+            //debug!("{}", name);
+            let (program, uprogram) = match get_program_by_name(&name) {
+                Some((program, uprogram)) => (program, uprogram),
+                None => {
+                    //FIXME: rename along with versions updates
+                    continue;
+                }
+            };
 
-        // let version = tag_name.to_string();
-        // if is_lib {
-        //     let version = LibraryVersion {
-        //         id: id.clone(),
-        //         name: name.clone(),
-        //         version,
-        //         documentation: "???".to_string(),
-        //     };
-        //     versions.push(UVersion::LibraryVersion(version));
-        // } else {
-        //     let version = ApplicationVersion {
-        //         id: id.clone(),
-        //         name: name.clone(),
-        //         version,
-        //     };
-        //     versions.push(UVersion::ApplicationVersion(version));
-        // }
+            let islib = matches!(uprogram, UProgram::Library(_));
+
+            if islib {
+                let version = LibraryVersion {
+                    id: program.id.clone(),
+                    name: name.clone(),
+                    version,
+                    documentation: "???".to_string(),
+                };
+                versions.push(UVersion::LibraryVersion(version));
+            } else {
+                let version = ApplicationVersion {
+                    id: program.id.clone(),
+                    name: name.clone(),
+                    version,
+                };
+                versions.push(UVersion::ApplicationVersion(version));
+            }
+        }
     }
 
     versions
 }
 
 /// for a given commit(version), walk all the package
-fn find_and_parse_cargo_toml<'repo>(
+fn parse_a_repo_of_a_version<'repo>(
     repo: &'repo Repository,
     tree: &'repo git2::Tree,
 ) -> Vec<Dependencies> {
@@ -77,49 +96,13 @@ fn find_and_parse_cargo_toml<'repo>(
                 .to_object(repo)
                 .expect("Failed to convert TreeEntry to Object");
             let blob = obj.as_blob().expect("Failed to interpret object as blob");
+
             let content =
                 std::str::from_utf8(blob.content()).expect("Cargo.toml content is not valid UTF-8");
 
-            //println!("xxx");
-            match content.parse::<Value>() {
-                Ok(toml) => {
-                    //println!("yyy: {:#?}", toml);
-                    if let Some(package) = toml.get("package") {
-                        if let Some(crate_name) = package.get("name") {
-                            //println!("zzz");
-                            let crate_name = crate_name.as_str().unwrap().to_string();
-                            let version = package
-                                .get("version")
-                                .unwrap()
-                                .as_str()
-                                .unwrap()
-                                .to_string();
+            let dependencies = parse_a_package_of_a_version(content).expect("failed to parse toml");
 
-                            let mut dependencies = vec![];
-
-                            if let Some(dep_table) = toml.get("dependencies") {
-                                if let Some(deps_table) = dep_table.as_table() {
-                                    for (name, val) in deps_table {
-                                        if let Some(version) = val.as_str() {
-                                            //FIXME:
-                                            dependencies.push((name.clone(), version.to_owned()));
-                                        }
-                                    }
-                                }
-                            }
-
-                            let dependencies = Dependencies {
-                                crate_name,
-                                version,
-                                dependencies,
-                            };
-                            println!("{:?}", dependencies);
-                            res.push(dependencies)
-                        }
-                    }
-                }
-                Err(_) => println!("Failed to parse Cargo.toml for {:?}", entry.name()),
-            }
+            res.push(dependencies);
 
             return TreeWalkResult::Ok; // Found the file, stop walking
         }
@@ -128,4 +111,54 @@ fn find_and_parse_cargo_toml<'repo>(
     .expect("Failed to walk the tree");
 
     res
+}
+
+fn parse_a_package_of_a_version(cargo_toml_content: &str) -> Option<Dependencies> {
+    //println!("xxx");
+    match cargo_toml_content.parse::<Value>() {
+        Ok(toml) => {
+            //println!("yyy: {:#?}", toml);
+            if let Some(package) = toml.get("package") {
+                if let Some(crate_name) = package.get("name") {
+                    //println!("zzz");
+                    let crate_name = crate_name.as_str().unwrap().to_string();
+                    let version = package
+                        .get("version")
+                        .unwrap()
+                        .as_str()
+                        .unwrap()
+                        .to_string();
+
+                    let mut dependencies = vec![];
+
+                    if let Some(dep_table) = toml.get("dependencies") {
+                        if let Some(deps_table) = dep_table.as_table() {
+                            for (name, val) in deps_table {
+                                if let Some(version) = val.as_str() {
+                                    //FIXME:
+                                    dependencies.push((name.clone(), version.to_owned()));
+                                } else if let Some(ver_tab) = val.as_table() {
+                                    if let Some(val) = ver_tab.get("version") {
+                                        if let Some(version) = val.as_str() {
+                                            dependencies.push((name.clone(), version.to_owned()));
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    let dependencies = Dependencies {
+                        crate_name,
+                        version,
+                        dependencies,
+                    };
+                    println!("{:?}", dependencies);
+                    return Some(dependencies);
+                }
+            }
+        }
+        Err(_) => println!("Failed to parse Cargo.toml for {:?}", cargo_toml_content),
+    }
+    None
 }
--- repo_import/src/utils.rs
+++ repo_import/src/utils.rs
@@ -1,17 +1,33 @@
 use lazy_static::lazy_static;
+use model::crate_info::{Program, UProgram};
 use std::collections::HashMap;
 use std::sync::Mutex;
 
 lazy_static! {
-    static ref GLOBAL_HASHMAP: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
+    pub static ref NAMESPACE_HASHMAP: Mutex<HashMap<String, String>> = Mutex::new(HashMap::new());
 }
 
-pub fn insert_info(key: String, value: String) {
-    let mut map = GLOBAL_HASHMAP.lock().unwrap();
+pub fn insert_namespace_by_repo_path(key: String, value: String) {
+    let mut map = NAMESPACE_HASHMAP.lock().unwrap();
     map.insert(key, value);
 }
 
-pub fn _get_info(key: &str) -> Option<String> {
-    let map = GLOBAL_HASHMAP.lock().unwrap();
+pub fn get_namespace_by_repo_path(key: &str) -> Option<String> {
+    let map = NAMESPACE_HASHMAP.lock().unwrap();
+    map.get(key).cloned()
+}
+
+lazy_static! {
+    pub static ref PROGRAM_HASHMAP: Mutex<HashMap<String, (Program, UProgram)>> =
+        Mutex::new(HashMap::new());
+}
+
+pub fn insert_program_by_name(key: String, value: (Program, UProgram)) {
+    let mut map = PROGRAM_HASHMAP.lock().unwrap();
+    map.insert(key, value);
+}
+
+pub fn get_program_by_name(key: &str) -> Option<(Program, UProgram)> {
+    let map = PROGRAM_HASHMAP.lock().unwrap();
     map.get(key).cloned()
 }
