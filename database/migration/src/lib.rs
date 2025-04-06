27ee8a68525be5e3eb3e3b9a041fce9d31c26d4b
xizheyin <xizheyin@smail.nju.edu.cn>
2025-04-06 12:49:44 +0800

    perf(database): add indices for query (#170)
    Signed-off-by: xizheyin <xizheyin@smail.nju.edu.cn>
    Co-authored-by: Tianxing Ye <benjamin.747@outlook.com>

--- database/migration/src/lib.rs
+++ database/migration/src/lib.rs
@@ -1,12 +1,16 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20250331_093145_init_crates_pro;
+mod m20250401_100000_add_performance_indices;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20250331_093145_init_crates_pro::Migration)]
+        vec![
+            Box::new(m20250331_093145_init_crates_pro::Migration),
+            Box::new(m20250401_100000_add_performance_indices::Migration),
+        ]
     }
 }
