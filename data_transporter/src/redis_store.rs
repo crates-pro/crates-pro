93ebd79e0943f7bbc49dbe2476699bbd4f39c704
hongwangliu-nju <1649209217@qq.com>
2025-04-24 15:46:56 +0800

    Change the cache of all UI pages from pg to redis
    Signed-off-by: hongwangliu-nju <1649209217@qq.com>

--- data_transporter/src/redis_store.rs
+++ data_transporter/src/redis_store.rs
@@ -27,7 +27,7 @@ impl RedisHandler {
     pub fn get_connection_mut(&mut self) -> &mut Connection {
         &mut self.connection
     }
-    pub async fn query_crates_info_from_redis(
+    pub async fn query_from_redis(
         &mut self,
         qid: String,
     ) -> Result<String, Box<dyn std::error::Error>> {
@@ -42,6 +42,7 @@ impl RedisHandler {
             }
         }
     }
+
     pub async fn insert_crates_info_into_redis(
         &mut self,
         namespace: String,
@@ -62,6 +63,93 @@ impl RedisHandler {
             .arg(7 * 24 * 60 * 60) // 一周的秒数
             .query(&mut self.connection)?;
         println!("finish insert crates_info");
+        Ok(())
+    }
+    pub async fn insert_dependency_into_redis(
+        &mut self,
+        namespace: String,
+        name: String,
+        version: String,
+        value: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("start insert dependency");
+        let key = format!("dependency:{}:{}:{}", namespace, name, version);
+
+        // 使用管道设置值和过期时间
+        let _: () = redis::pipe()
+            .cmd("SET")
+            .arg(&key)
+            .arg(&value)
+            .cmd("EXPIRE")
+            .arg(&key)
+            .arg(7 * 24 * 60 * 60) // 一周的秒数
+            .query(&mut self.connection)?;
+        println!("finish insert dependency");
+        Ok(())
+    }
+    pub async fn insert_dependency_graph_into_redis(
+        &mut self,
+        namespace: String,
+        name: String,
+        version: String,
+        value: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("start insert dependencygraph");
+        let key = format!("dependencygraph:{}:{}:{}", namespace, name, version);
+
+        // 使用管道设置值和过期时间
+        let _: () = redis::pipe()
+            .cmd("SET")
+            .arg(&key)
+            .arg(&value)
+            .cmd("EXPIRE")
+            .arg(&key)
+            .arg(7 * 24 * 60 * 60) // 一周的秒数
+            .query(&mut self.connection)?;
+        println!("finish insert dependencygraph");
+        Ok(())
+    }
+    pub async fn insert_dependent_into_redis(
+        &mut self,
+        namespace: String,
+        name: String,
+        version: String,
+        value: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("start insert dependent");
+        let key = format!("dependent:{}:{}:{}", namespace, name, version);
+
+        // 使用管道设置值和过期时间
+        let _: () = redis::pipe()
+            .cmd("SET")
+            .arg(&key)
+            .arg(&value)
+            .cmd("EXPIRE")
+            .arg(&key)
+            .arg(7 * 24 * 60 * 60) // 一周的秒数
+            .query(&mut self.connection)?;
+        println!("finish insert dependent");
+        Ok(())
+    }
+    pub async fn insert_versionpage_into_redis(
+        &mut self,
+        namespace: String,
+        name: String,
+        value: String,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("start insert versionpage");
+        let key = format!("versionpage:{}:{}", namespace, name);
+
+        // 使用管道设置值和过期时间
+        let _: () = redis::pipe()
+            .cmd("SET")
+            .arg(&key)
+            .arg(&value)
+            .cmd("EXPIRE")
+            .arg(&key)
+            .arg(7 * 24 * 60 * 60) // 一周的秒数
+            .query(&mut self.connection)?;
+        println!("finish insert versionpage");
         Ok(())
     }
 }
